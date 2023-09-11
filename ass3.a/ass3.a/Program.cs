1e9c832fdabd95889e5b151e3d6a75de9a35b18c
Varsha Nanjegowda <varshan@infinite.com>
2023-09-11 11:02:18 +0530

    Assignments

---
+++ ass3.a/ass3.a/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word");
+            string name = Console.ReadLine();
+            Console.WriteLine(name.Length);
+            Console.Read();
+
+        }
+    }
+}
---
+++ ass3.b/ass3.b/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass3.b
+{
+    class Reverse
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENTER A WORD");
+            string str = Console.ReadLine();
+            char[] stringArray = str.ToCharArray();
+            Array.Reverse(stringArray);
+            string reversedStr = new string(stringArray);
+            Console.WriteLine($"Actual string {str}\n");
+            Console.WriteLine($"Reversed string {reversedStr} ");
+            Console.ReadLine();
+        }
+    }
+}
