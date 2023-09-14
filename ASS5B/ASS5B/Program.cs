a3dbf7ca064421553a137ea96e8428cbf9ef8af3
Varsha Nanjegowda <varshan@infinite.com>
2023-09-14 21:21:59 +0530

    ass5b

---
+++ ASS5B/ASS5B/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASS5B
+{
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a string: ");
+            string String = Console.ReadLine();
+
+            Console.WriteLine("Enter a letter to count: ");
+            char Count = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            int count = Occurrences(String, Count);
+
+            Console.WriteLine($"The letter '{Count}' appears {count} times in the string.");
+            Console.ReadLine();
+        }
+
+        static int Occurrences(string str, char tem)
+        {
+            int count = 0;
+
+            foreach (char letter in str)
+            {
+                if (letter == tem)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+
+}
+    
