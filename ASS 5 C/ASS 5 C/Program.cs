c72d1930b7efd1978a9fa8492d6aba4f37b7d761
Varsha Nanjegowda <varshan@infinite.com>
2023-09-14 21:34:17 +0530

    ass5c

---
+++ ASS 5 C/ASS 5 C/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASS_5_C
+{
+
+
+    class SaleDetails
+    {
+        private int SalesNum;
+        private int ProductNum;
+        private double Price;
+        private DateTime DOfSale;
+        private int Qty;
+        private double TotalAmount;
+
+
+        public SaleDetails(int salesNo, int productNo, double price, int qty, DateTime dateOfSale)
+        {
+            this.SalesNum = salesNo;
+            this.ProductNum = productNo;
+            this.Price = price;
+            this.DOfSale = dateOfSale;
+            this.Qty = qty;
+            this.TotalAmount = 0;
+        }
+
+
+        public void Sales()
+        {
+            this.TotalAmount = this.Qty * this.Price;
+        }
+
+
+        public void ShowData()
+        {
+            Console.WriteLine($"Sales No: {this.SalesNum}");
+            Console.WriteLine($"Product No: {this.ProductNum}");
+            Console.WriteLine($"Price: {this.Price:C}");
+            Console.WriteLine($"Date of Sale: {this.DOfSale.ToShortDateString()}");
+            Console.WriteLine($"Qty: {this.Qty}");
+            Console.WriteLine($"Total Amount: {this.TotalAmount:C}");
+        }
+    }
+
+}
+    
---
+++ ASS 5 C/ASS 5 C/class3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASS_5_C
+{
+    class class3
+    {
+        public static void Main()
+        {
+            SaleDetails sale = new SaleDetails(2, 342, 14.99, 3, DateTime.Now);
+            sale.Sales(); // Calculate the TotalAmount
+
+            Console.WriteLine("Sales Details:");
+            sale.ShowData();
+            Console.ReadLine();
+        }
+    }
+}
+
