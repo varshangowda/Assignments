3334eef052003f0f6dc986b8bb54e110c6c0476d
Varsha Nanjegowda <varshan@infinite.com>
2023-09-22 12:40:41 +0530

    My git

---
+++ ASSIGNMENTS_c#/assignment6/ASS 6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASS_6
+{
+    namespace ConcessionApp
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Enter your name:");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Enter your age:");
+                if (int.TryParse(Console.ReadLine(), out int age))
+                {
+                    var calculator = new Concession.ConcessionCalculator
+                    {
+                        Name = name,
+                        Age = age
+                    };
+
+                    calculator.CalculateConcession();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid age input. Please enter a valid age.");
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ASSIGNMENTS_c#/assignment6/Concession/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concession
+{
+    public class ConcessionCalculator
+    {
+        public const int TotalFare = 500;
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public void CalculateConcession()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+            if (Age <= 5)
+            {
+                
+                Console.WriteLine("Little Champs - Free Ticket");
+            }
+            else if (Age > 60)
+            {
+                double discountedFare = TotalFare * 0.7;
+                
+                Console.WriteLine($"Senior Citizen - Fare: {discountedFare:C}");
+            }
+            else
+            {
+               
+                Console.WriteLine($"Ticket Booked - Fare: {TotalFare:C}");
+            }
+        }
+    }
+}
