8e6f5f6ce24b0af0b81fbaf53e2347519c3e6581
Varsha Nanjegowda <varshan@infinite.com>
2023-09-25 13:15:44 +0530

    code test

---
+++ Code_Base_Test/C SHARP CODE BASED TEST 3 B/C SHARP CODE BASED TEST 3 B/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_SHARP_CODE_BASED_TEST_3_B
+{
+    class Box
+    {
+        public double length;
+        public double breadth;
+        public Box(double Length, double Breadth)
+        {
+            length = Length;
+            breadth = Breadth;
+        }
+
+        public static Box AddBoxes(Box box1, Box box2)
+        {
+            double sumLength = box1.length + box2.length;
+            double sumbreadth = box1.breadth + box2.breadth;
+            return new Box(sumLength, sumbreadth);
+        }
+
+        public void displaydetails()
+        {
+            Console.WriteLine(" BOX OBJECT DETAILS");
+            Console.WriteLine("LENGTH IS:" + length);
+            Console.WriteLine("BREADTH IS:" + breadth);
+        }
+    }
+        class Test
+        { static void Main(string[] args)
+            {
+            Console.Write("Enter the length of Box1: ");
+            double length1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the breadth of Box1: ");
+            double breadth1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the length of Box2: ");
+            double length2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the breadth of Box2: ");
+            double breadth2 = Convert.ToDouble(Console.ReadLine());
+            Box box1 = new Box(length1, breadth1);
+            Box box2 = new Box(length2, breadth2);
+
+            Box box3 = Box.AddBoxes(box1, box2);
+            Console.WriteLine("Details of Box1:");
+            box1.displaydetails();
+            Console.WriteLine("Details of Box2:");
+            box2.displaydetails();
+            Console.WriteLine("Details of Box 3 (Sum of Box1 and Box2  is New Box ):");
+            box3.displaydetails();
+            Console.ReadLine();
+        }
+    }
+}   
+
---
+++ Code_Base_Test/C SHARP CODE BASED TEST 3/Codebase test 3a/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codebase_test_3a
+{
+    class Cricket
+    {
+        public void PointsCalculation(int no_of_matches)
+        {
+            int sum = 0;
+            double average;
+            Console.WriteLine("ENTER THE SCORES FOR EACH MATCH");
+            for (int i = 1; i < no_of_matches; i++)
+            {
+                Console.WriteLine("SCORE of a match" + i + ":");
+                int score = int.Parse(Console.ReadLine());
+                sum += score;
+            }
+            average = (double)sum / no_of_matches;
+            Console.WriteLine("SUM OF SCORES:" + sum);
+            Console.WriteLine("AVERAGE SCORE:" + average);
+        }
+    }
+
+    class Ipl
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENTER THE N OF MATCHES");
+            int no_of_matches = int.Parse(Console.ReadLine());
+            Cricket cricket = new Cricket();
+            cricket.PointsCalculation(no_of_matches);
+            Console.ReadLine();
+        }
+    }
+}
