6cf566c17a6ffad42c27834d6a1f8143e6309735
Varsha Nanjegowda <varshan@infinite.com>
2023-10-23 13:12:44 +0530

    6

---
+++ Code_Base_Test/CODE BASED TEST 6/QUE1/ABC/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EmployeeManagementApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Server=ICS-LT-HVM1DN3\MSSQLSERVER02;Database=ASS2;Trusted_Connection=True;";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("AddEmployee2", connection))
+                {
+                    Console.WriteLine("Enter Emlpoyee Name");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Enter the Employee Salary");
+                    decimal sal = Convert.ToDecimal(Console.ReadLine());
+                    Console.WriteLine("Employee Type P or F");
+                    char type = Convert.ToChar(Console.Read());
+
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@empname", name);
+                    command.Parameters.AddWithValue("@empsal", sal);
+                    command.Parameters.AddWithValue("@emptype", type);
+
+                    command.ExecuteNonQuery();
+                }
+
+            }
+
+            Console.WriteLine("Employee added successfully.");
+
+            // Display all records
+            DisplayAllEmployeeRecords(connectionString);
+            Console.Read();
+        }
+
+        // Define the DisplayAllEmployeeRecords method outside of Main
+        static void DisplayAllEmployeeRecords(string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("SELECT * FROM Code_Employee2", connection))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"Employee ID: {reader["empno"]}, Name: {reader["empname"]}, Salary: {reader["empsal"]}, Type: {reader["emptype"]}");
+                    }
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
--- Code_Base_Test/CODE BASED TEST 6/QUE1/ABC/Properties/AssemblyInfo.cs
+++ Code_Base_Test/CODE BASED TEST 6/QUE1/ABC/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ADO Codebase 6 c,d")]
+[assembly: AssemblyTitle("ABC")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ADO Codebase 6 c,d")]
+[assembly: AssemblyProduct("ABC")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3e6cc521-9856-4479-9e40-8999bb04a2ea")]
+[assembly: Guid("464a272c-7bf0-4751-a0bc-eb21dbe96c3a")]
 
 // Version information for an assembly consists of the following four values:
 //
