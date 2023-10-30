6f183e6b8ee39fde74a2a431439d495323553a23
Varsha Nanjegowda <varshan@infinite.com>
2023-10-30 11:34:16 +0530

    test7

---
+++ Code_Base_Test/CODE BASED TEST 7/Codebased Asp_code Q7)/Codebased Asp_code Q7)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="Codebased_Asp_code_Q7_.Global_asax" Language="vb" %>
---
+++ Code_Base_Test/CODE BASED TEST 7/Codebased test 7 1)/Codebased test 7 1)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codebased_test_7_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
